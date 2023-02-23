2852a175053e9cb2880096d86ecfa52f231c61c5
BenediktWz <benedikt.woerz@gmail.com>
2023-02-23 16:59:15 +0100

    package grasp_planner

--- setup.py
+++ setup.py
@@ -7,11 +7,5 @@ setup(
     license="MIT",
     author="Autonomous Learning Robots @ KIT",
     url="https://alr.anthropomatik.kit.edu/",
-    package_data={"models": ["*"]},
     packages=find_packages(),
-    install_requires=[
-        "numpy<1.24",  # ros_numpy does not support numpy 1.24 yet: https://github.com/eric-wieser/ros_numpy/pull/32
-        "pyyaml",
-        "trimesh",
-    ],
 )
