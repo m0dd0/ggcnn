7a6ce8c55745cb2a502dd0dd1f01b7379704a99b
BenediktWz <benedikt.woerz@gmail.com>
2023-02-24 17:30:12 +0100

    Fixed 6D transformation problems

--- ggcnn_grasp_planner_pckg/ggcnn_grasp_planner.py
+++ ggcnn_grasp_planner_pckg/ggcnn_grasp_planner.py
@@ -21,15 +21,23 @@ import matplotlib.pyplot as plt
 
 from .utils.dataset_processing import grasp, image
 
-# weights and model -> weights probably not needed
-# TODO: define model path universal ./
-weights_path = '/home/i53/student/b_woerz/ggcnn2/ggcnn_weights_cornell/ggcnn_epoch_23_cornell_statedict.pt'
-model_path = '../ggcnn_weights_cornell/ggcnn_epoch_23_cornell'
-#'/home/i53/student/b_woerz/ggcnn2/ggcnn_weights_cornell/ggcnn_epoch_23_cornell'
+from pathlib import Path
+
+
+
+model_path = Path(__file__).parent / 'ggcnn_weights_cornell/ggcnn_epoch_23_cornell'
+weights_path = Path(__file__).parent / 'ggcnn_weights_cornell/ggcnn_epoch_23_cornell_statedict.pt'
+
+
+# not possible to load model directly because serialized data is bound to 
+# the specific classes and the exact directory structure used when the model is saved
+net = GGCNN()
+net.load_state_dict(torch.load(weights_path))
+device = torch.device("cuda:0")
+net.cuda()
+
 
 depth_img_path = '/home/i53/student/b_woerz/Documents/ycb_sim_data_1/ycb_simulation_sample_002_cracker_box/depth_img.npy'
-#'/home/i53/student/b_woerz/Documents/ycb_sim_data_1/ycb_simulation_sample_002_cracker_box/depth_img.npy'
-#'/home/i53/student/b_woerz/Documents/ycb_sim_data_1/ycb_simulation_sample_001_master_chef_can/depth_img.npy'
 
 # rotation always 0
 rotation = 0
@@ -45,8 +53,8 @@ cam_pos_input = [0.5, 0.0, 1.0]
 cam_quat_input = [0.7071067811865476, 0.0, 0.0, -0.7071067811865475]
 
  # loading image -> will not be necessary in simulation
-depth_image_instance = image.DepthImage.from_npy(depth_img_path)
-depth_image = depth_image_instance.img
+local_depth_image_instance = image.DepthImage.from_npy(depth_img_path)
+local_depth_image = local_depth_image_instance.img
 
 #determine how many grasps to output
 number_grasps = 10
@@ -59,12 +67,9 @@ def _get_crop_attrs():
         return center, left, top
 
 def preprocessing(depth_img, rot, zoom):
-        #TODO: apply mask to get rid of table edge
+        #TODO: define mask more universal
         og_depth_img = image.DepthImage(depth_img.img)
         
-        
-        imgplot3 = plt.imshow(depth_img.img)
-        plt.show()
         center, left, top = _get_crop_attrs()
         depth_img.rotate(rot, center)
         depth_img.crop((top, left), (min(480, top + output_size), min(640, left + output_size)))
@@ -73,8 +78,7 @@ def preprocessing(depth_img, rot, zoom):
         depth_img.mask(mask_value = 1.5, set_value = 1.02)
 
         depth_img.normalise()
-        imgplot5 = plt.imshow(depth_img.img)
-        plt.show()
+
 
         depth_img.zoom(zoom)
         depth_img.resize((output_size, output_size))
@@ -87,21 +91,18 @@ def numpy_to_torch(s):
             return torch.from_numpy(s.astype(np.float32))
 
 def get_ggcnn_output(depth_img, model_path = model_path):
-    #Load Network
-    net = torch.load(model_path)
-    device = torch.device("cuda:0")
+    
 
     with torch.no_grad():
         
         depth_imagec = depth_img.to(device)
             
         pred = net.forward(depth_imagec) #-> pos_output, cos_output, sin_output, width_output
-        print('PREDICTION: Type: {}, Shape: {}'.format(type(pred), len(pred)))
+
         q_img, ang_img, width_img = post_process_output(pred['pos'], pred['cos'], pred['sin'], pred['width'] )
         return q_img, ang_img, width_img
 
 def uncrop_2D_grasps(grasps2D_list):
-     #TODO: postrpocess the output images (uncrop)
     crop_center, left, top = _get_crop_attrs()
     for g in grasps2D_list:
         g.center = list(g.center)  # Convert tuple to list
@@ -161,16 +162,23 @@ def transform_2D_grasp_to_6D(g, cam_intrinsics, cam_position, cam_quat):
     fy = cam_intrinsics[1][1]
 
     #grasp position in image coordinates
-    #vielleicht andersrum - erster Wert ist y zweiter ist x
-    p_img_x = g.center[1]
-    p_img_y = g.center[0]
-    
+
+    p_img_x = g.center[1] # u
+    p_img_y = g.center[0] # v
+    print(f"Position Image Perspective (x, y): ({p_img_x},{p_img_y})")
+
     #grasp position in cam coordinates
-    p_cam_z = g.depth
+    """
+    Information from Camera Base Class.
+    The Camera looks along its -Z axis
+    +X goes to image right, +Y image up.
+    """
+    p_cam_z = g.depth # minus because camera looks along -Z ??
     p_cam_x = (p_img_x - cx) * p_cam_z / fx
     p_cam_y = (p_img_y - cy) * p_cam_z / fy
 
     p_cam = [p_cam_x, p_cam_y, p_cam_z]
+    print(f"Position Cam Perspective (x,y,z): {p_cam}")
     p_cam = np.array(p_cam).reshape((3, 1))
 
     #cam_quat in rotationsmatrix umstellen (cam_rot)
@@ -182,11 +190,15 @@ def transform_2D_grasp_to_6D(g, cam_intrinsics, cam_position, cam_quat):
     cam_pos = np.array(cam_position).reshape((3, 1))
     
     p_world = cam_rot_inv @ p_cam + cam_pos
+    print(f"Position World Perspective (x,y,z): {p_world}")
     
     p_world = p_world.flatten()
 
     #grasp orientation in world coordinates
-    q_world = euler_to_quaternion(0, 0, g.angle)
+    # rotate 180 degrees around z axis and take negative value of the angle, 
+    # because the angle is defined as positive value (relative to x axis) but points in negative direction
+   
+    q_world = euler_to_quaternion(np.pi , 0, -g.angle)
 
     return p_world, q_world
 
@@ -199,45 +211,44 @@ def get_6D_grasps(grasps2D_list, camera_intrinsics, cam_pos, cam_quat):
     return grasps6D
 
 
-def ggcnn_get_grasp(depth_image, camera_intrinsics, cam_pos, cam_quat, rotation=0.0, zoom=1.0, output_size = 300, weights_path = './ggcnn_weights_cornell/ggcnn_epoch_23_cornell_statedict.pt'):
+def ggcnn_get_grasp(depth_image, camera_intrinsics, cam_pos, cam_quat, rotation=0.0, zoom=1.0, output_size = 300):
 
     depth_img_inst = image.DepthImage(depth_image)
 
     #preprocessing image
     og_depth_img, preprocessed_depth_image = preprocessing(depth_img_inst, rotation, zoom)
-    
-    print('DEPTH IMAGE 2: Type: {}, Shape: {}'.format(type(preprocessed_depth_image), preprocessed_depth_image.shape))
-    imgplot2 = plt.imshow(preprocessed_depth_image)
-    #plt.show()
 
     #passing image through ggcnn -> (300 X 300) images for position, angle, width
     depth_image_tens = numpy_to_torch(preprocessed_depth_image)
     q_img, ang_img, width_img = get_ggcnn_output(depth_image_tens)
 
-    print('Position IMAGE: Type: {}, Shape: {}'.format(type(q_img), q_img.shape))
-    imgplot3 = plt.imshow(q_img)
-    plt.show()
-
-    #derive performable grasp pose -> position in cartesian coordinates [x,y,z], orientation as quaternion [x0, x1, x2, x3]
+    #derive performable grasp pose -> position in cartesian coordinates [x,y,z], 
+    # orientation as quaternion [x0, x1, x2, x3]
     
     # hier wird auch depth Wert initialisiert
     grasps2D = detect_grasps(og_depth_img, q_img, ang_img, no_grasps=number_grasps)
     print('2D GRASP Center: {}, Angle: {}'.format(grasps2D[0].center, grasps2D[0].angle))
     grasps2D = uncrop_2D_grasps(grasps2D)
     print('UNCROPPED 2D GRASP Center: {}'.format(grasps2D[0].center))
-    #TODO: Achtung Tiefenwert einfach aus q_img
+
     grasps6D = get_6D_grasps(grasps2D, camera_intrinsics, cam_pos, cam_quat)
     print('6D GRASP: position: {}, orientation: {}'.format(grasps6D[0].position, grasps6D[0].orientation))
     
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    axs[0].imshow(depth_image)
+    axs[0].plot(grasps2D[0].center[1], grasps2D[0].center[0], 'ro')
+    helper_point = [grasps2D[0].center[0] + 30*np.sin(grasps2D[0].angle), grasps2D[0].center[1] + 50*np.cos(grasps2D[0].angle)]
+    axs[0].plot([grasps2D[0].center[1], helper_point[1]], [grasps2D[0].center[0], helper_point[0]], 'b-')
+    axs[1].imshow(q_img)
+    plt.suptitle('Position:{}    Angle: {}'.format(grasps2D[0].center, np.rad2deg(grasps2D[0].angle)))
+    plt.show()
+
     return grasps6D
      
 
-      
-      
-
 if __name__ == '__main__':
     
-    grasps6D_list = ggcnn_get_grasp(depth_image, camera_intrinsics_input, cam_pos_input, cam_quat_input)
+    grasps6D_list = ggcnn_get_grasp(local_depth_image, camera_intrinsics_input, cam_pos_input, cam_quat_input)
     
 
     
